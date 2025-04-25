006ca292a36170585db1bf04b6309b8b5332f349
Oscar Aspelin <130501807+OscarAspelin95@users.noreply.github.com>
2025-04-25 17:28:32 +0200

    Improve pattern generation to be more dynamic. Also, add 5x5 grid option and VI Dan

--- pattern_bingo/src/components/bingo.rs
+++ pattern_bingo/src/components/bingo.rs
@@ -1,11 +1,45 @@
 use dioxus::prelude::*;
-use rand::seq::SliceRandom;
+use rand::seq::{IndexedRandom, SliceRandom};
 
 const RANDOMIZE_SVG: Asset = asset!("/assets/randomize.svg");
 const TTKD_LOGO: Asset = asset!("/assets/ttkd_logo.jpeg");
 
 // TODO - add reset/reload button. Keep belt choice but randomize patterns and reset button toggles
 // TODO - add option for 3x3 grid, 4x4 grid or all patterns.
+
+static PATTERNS: &'static [&str; 24] = &[
+    // 1-Kup
+    "Chon-Ji",
+    "Dan-Gun",
+    "Do-San",
+    "Won-Hyo",
+    "Yul-Gok",
+    "Choong-Gun",
+    "Toi-Gye",
+    "Hwa-Rang",
+    "Choong-Moo",
+    // 1-Dan
+    "Kwan-Gae",
+    "Po-Eun",
+    "Ge-Baek",
+    // 2-Dan
+    "Eui-Am",
+    "Choong-Jang",
+    "Juche",
+    // 3-Dan
+    "Sam-Il",
+    "Yoo-Sin",
+    "Choi-Yong",
+    // 4-Dan
+    "Yon-Gae",
+    "Ul-Ji",
+    "Moon-Moo",
+    // 5-Dan
+    "So-San",
+    "Se-Jong",
+    // 6-Dan
+    "Tong-Il",
+];
 
 #[component]
 pub fn Bingo() -> Element {
@@ -30,79 +64,8 @@ pub struct Refresh {
     state: Signal<bool>,
 }
 
-#[derive(Debug, Clone)]
-pub struct GridSize {
-    current_size: Signal<String>,
-    options: Signal<Vec<String>>,
-}
-
 #[component]
 fn BingoGrid() -> Element {
-    // Tmp solution
-    // 1 Kup.
-
-    let mut bingo_grid_1kup: Vec<String> = vec![
-        "Chon-Ji".to_string(),
-        "Dan-Gun".to_string(),
-        "Do-San".to_string(),
-        "Won-Hyo".to_string(),
-        "Yul-Gok".to_string(),
-        "Choong-Gun".to_string(),
-        "Toi-Gye".to_string(),
-        "Hwa-Rang".to_string(),
-        "Choong-Moo".to_string(),
-    ];
-
-    // 1 Dan.
-    let mut bingo_grid_1dan: Vec<String> = [
-        bingo_grid_1kup.clone(),
-        vec![
-            "Kwan-Gae".to_string(),
-            "Po-Eun".to_string(),
-            "Ge-Baek".to_string(),
-        ],
-    ]
-    .concat();
-
-    // 2 Dan.
-    let mut bingo_grid_2dan: Vec<String> = [
-        bingo_grid_1dan.clone(),
-        vec![
-            "Eui-Am".to_string(),
-            "Choong-Jang".to_string(),
-            "Juche".to_string(),
-        ],
-    ]
-    .concat();
-
-    // 3 Dan.
-    let mut bingo_grid_3dan: Vec<String> = [
-        bingo_grid_2dan.clone(),
-        vec![
-            "Sam-Il".to_string(),
-            "Yoo-Sin".to_string(),
-            "Choi-Yong".to_string(),
-        ],
-    ]
-    .concat();
-
-    // 4 Dan.
-    let mut bingo_grid_4dan: Vec<String> = [
-        bingo_grid_3dan.clone(),
-        vec![
-            "Yon-Gae".to_string(),
-            "Ul-Ji".to_string(),
-            "Moon-Moo".to_string(),
-        ],
-    ]
-    .concat();
-
-    let mut bingo_grid_5dan: Vec<String> = [
-        bingo_grid_4dan.clone(),
-        vec!["So-San".to_string(), "Se-Jong".to_string()],
-    ]
-    .concat();
-
     let mut belt_choice = use_context_provider(|| BeltChoice {
         choice: Signal::new("1-Kup".to_string()),
     });
@@ -113,7 +76,7 @@ fn BingoGrid() -> Element {
 
     let mut grid_size = use_signal(|| "3x3".to_string());
 
-    let mut bingo_grid = use_signal(|| bingo_grid_1kup.clone());
+    let mut bingo_grid = use_signal(|| PATTERNS[..9].to_vec());
 
     use_effect(move || {
         let choice = belt_choice.choice.read();
@@ -122,41 +85,84 @@ fn BingoGrid() -> Element {
         let s = choice.as_str();
         let mut rng = rand::rng();
 
+        let num_patterns: usize;
+
         match s {
             "1-Kup" => {
-                grid_size.set("3x3".to_string());
-                bingo_grid_1kup.shuffle(&mut rng);
-                bingo_grid.set(bingo_grid_1kup.clone());
+                num_patterns = 9;
             }
             "1-Dan" => {
-                grid_size.set("3x3".to_string());
-                bingo_grid_1dan.shuffle(&mut rng);
-                bingo_grid.set(bingo_grid_1dan[..9].to_vec());
+                num_patterns = 12;
             }
             "2-Dan" => {
-                grid_size.set("3x3".to_string());
-                bingo_grid_2dan.shuffle(&mut rng);
-                bingo_grid.set(bingo_grid_2dan[..9].to_vec());
+                num_patterns = 15;
             }
             "3-Dan" => {
-                grid_size.set("4x4".to_string());
-                bingo_grid_3dan.shuffle(&mut rng);
-                bingo_grid.set(bingo_grid_3dan[..16].to_vec());
+                num_patterns = 18;
             }
             "4-Dan" => {
-                grid_size.set("4x4".to_string());
-                bingo_grid_4dan.shuffle(&mut rng);
-                bingo_grid.set(bingo_grid_4dan[..16].to_vec());
+                num_patterns = 21;
             }
             "5-Dan" => {
-                grid_size.set("4x4".to_string());
-                bingo_grid_5dan.shuffle(&mut rng);
-                bingo_grid.set(bingo_grid_5dan[..16].to_vec());
+                num_patterns = 23;
+            }
+            "6-Dan" => {
+                num_patterns = 24;
             }
 
             _ => {
                 panic!("Invalid belt choice")
             }
+        }
+
+        // First, extract available patterns in order.
+        let mut available_patterns = PATTERNS[..num_patterns].to_vec();
+
+        // Randomly shuffle.
+        available_patterns.shuffle(&mut rng);
+
+        // Then we need to extract only 3x3 or 4x4 patterns
+        let grid_size_choice = grid_size.read();
+        let gs_as_str = grid_size_choice.as_str();
+
+        match gs_as_str {
+            // For 3x3 we are safe since every belt has at least 9 patterns available.
+            "3x3" => {
+                bingo_grid.set(available_patterns[..9].to_vec());
+            }
+            // For all other grid sizes, we need to backfill.
+            "4x4" => {
+                //
+                if available_patterns.len() < 16 {
+                    // Backfill until we have the desired amount of patterns.
+                    let mut secondary_set = available_patterns.clone();
+                    secondary_set.shuffle(&mut rng);
+
+                    // This should be safe regardless of grid size.
+                    while available_patterns.len() < 16 {
+                        available_patterns.push(secondary_set.choose(&mut rng).unwrap());
+                    }
+                }
+
+                bingo_grid.set(available_patterns[..16].to_vec());
+            }
+            "5x5" => {
+                //
+                if available_patterns.len() < 25 {
+                    // Backfill until we have the desired amount of patterns.
+                    let mut secondary_set = available_patterns.clone();
+                    secondary_set.shuffle(&mut rng);
+
+                    // This should be safe regardless of grid size.
+                    while available_patterns.len() < 25 {
+                        available_patterns.push(secondary_set.choose(&mut rng).unwrap());
+                    }
+                }
+
+                bingo_grid.set(available_patterns[..25].to_vec());
+            }
+
+            _ => panic!(""),
         }
 
         dbg!("{}", choice);
@@ -179,6 +185,21 @@ fn BingoGrid() -> Element {
                 option { value: "3-Dan", "III Dan" }
                 option { value: "4-Dan", "IV Dan" }
                 option { value: "5-Dan", "V Dan" }
+                option { value: "6-Dan", "VI Dan" }
+            }
+
+            label { r#for: "select-grid", "Välj storlek:" }
+            select {
+                name: "select-grid",
+                id: "select-grid",
+                onchange: move |evt| {
+                    grid_size.set(evt.value());
+                },
+                // List all options here.
+                option { value: "3x3", "3x3" }
+                option { value: "4x4", "4x4" }
+                option { value: "5x5", "5x5" }
+
             }
 
             button {
